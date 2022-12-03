04832e9309079f685bfdf4508a048e7d13ba272f
Kerem-Kaplan <keremkaplan1000@gmail.com>
2022-12-03 14:56:57 +0300

    used cookie and tested

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -3,21 +3,20 @@ import jwt from "jsonwebtoken";
 
 const authenticateToken = async (req, res, next) => {
   try {
-    const authHeader = req.headers["authorization"];
-    const token = authHeader && authHeader.split(" ")[1];
-
-    if (!token) {
-      return res.status(401).json({
-        succeed: false,
-        error: "No token available",
-      });
+    const token=req.cookies.jwt
+    if(token){
+        jwt.verify(token,process.env.JWT_SECRET,(err)=>{
+            if(err){
+                console.log(err.message);
+                res.redirect("/login")
+            }else{
+                next()
+            }
+        })
+    }else{
+        res.redirect("/login")
     }
-
-    req.user = await User.findById(
-      jwt.verify(token, process.env.JWT_SECRET).userId
-    );
-
-    next();
+   
   } catch (error) {
     res.status(401).json({
         succeed:false,
