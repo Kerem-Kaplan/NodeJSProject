8c6ea623dd2ed63719bc6f544ade776a995b533b
Kerem-Kaplan <keremkaplan1000@gmail.com>
2022-12-08 21:58:20 +0300

    edited follower and following actions

--- controllers/userController.js
+++ controllers/userController.js
@@ -19,7 +19,6 @@ const createUser = async (req, res) => {
       });
     }
 
-
     res.status(400).json(errors2);
   }
 };
@@ -71,15 +70,20 @@ const createToken = (userId) => {
 
 const getDashboardPage = async (req, res) => {
   const photos = await Photo.find({ user: res.locals.user._id });
+  const user = await User.findById({ _id: res.locals.user._id }).populate([
+    "followings",
+    "followers",
+  ]);
   res.render("dashboard", {
     link: "dashboard",
     photos,
+    user
   });
 };
 
 const getAllUsers = async (req, res) => {
   try {
-    const users = await User.find({_id:{$ne : res.locals.user._id}});
+    const users = await User.find({ _id: { $ne: res.locals.user._id } });
     res.status(200).render("users", {
       users,
       link: "users",
@@ -94,12 +98,18 @@ const getAllUsers = async (req, res) => {
 
 const getAUser = async (req, res) => {
   try {
-    const user = await User.findById({_id:req.params.id});
-    const photos = await Photo.find({user:res.locals.user._id});
+    const user = await User.findById({ _id: req.params.id });
+
+    const inFollowers = user.followers.some((followers) => {
+      return followers.equals(res.locals.user._id);
+    });
+
+    const photos = await Photo.find({ user: user._id });
     res.status(200).render("user", {
       user,
       photos,
       link: "users",
+      inFollowers,
     });
   } catch (error) {
     res.status(500).json({
@@ -109,4 +119,66 @@ const getAUser = async (req, res) => {
   }
 };
 
-export { createUser, loginUser, getDashboardPage,getAllUsers,getAUser };
+const follow = async (req, res) => {
+  try {
+    let user = await User.findByIdAndUpdate(
+      { _id: req.params.id },
+      {
+        $push: { followers: res.locals.user._id },
+      },
+      { new: true }
+    );
+
+    user = await User.findByIdAndUpdate(
+      { _id: res.locals.user._id },
+      {
+        $push: { followings: req.params.id },
+      },
+      { new: true }
+    );
+
+    res.status(200).redirect(`/users/${req.params.id}`)
+  } catch (error) {
+    res.status(500).json({
+      succesed: false,
+      error,
+    });
+  }
+};
+
+const unfollow = async (req, res) => {
+  try {
+    let user = await User.findByIdAndUpdate(
+      { _id: req.params.id },
+      {
+        $pull: { followers: res.locals.user._id },
+      },
+      { new: true }
+    );
+
+    user = await User.findByIdAndUpdate(
+      { _id: res.locals.user._id },
+      {
+        $pull: { followings: req.params.id },
+      },
+      { new: true }
+    );
+
+    res.status(200).redirect(`/users/${req.params.id}`)
+  } catch (error) {
+    res.status(500).json({
+      succesed: false,
+      error,
+    });
+  }
+};
+
+export {
+  createUser,
+  loginUser,
+  getDashboardPage,
+  getAllUsers,
+  getAUser,
+  follow,
+  unfollow,
+};
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,11 +1,23 @@
 import Express from "express";
 import * as userController from "../controllers/userController.js";
-import * as authMiddleware from '../middlewares/authMiddleware.js'
+import * as authMiddleware from "../middlewares/authMiddleware.js";
 const router = Express.Router();
 
-router.route("/register").post(userController.createUser)
-router.route("/login").post(userController.loginUser)
-router.route("/dashboard").get(authMiddleware.authenticateToken,userController.getDashboardPage)
-router.route("/").get(authMiddleware.authenticateToken,userController.getAllUsers)
-router.route("/:id").get(authMiddleware.authenticateToken,userController.getAUser)
+router.route("/register").post(userController.createUser);
+router.route("/login").post(userController.loginUser);
+router
+  .route("/dashboard")
+  .get(authMiddleware.authenticateToken, userController.getDashboardPage);
+router
+  .route("/")
+  .get(authMiddleware.authenticateToken, userController.getAllUsers);
+router
+  .route("/:id")
+  .get(authMiddleware.authenticateToken, userController.getAUser);
+router
+  .route("/:id/follow")
+  .put(authMiddleware.authenticateToken, userController.follow);
+router
+  .route("/:id/unfollow")
+  .put(authMiddleware.authenticateToken, userController.unfollow);
 export default router;
